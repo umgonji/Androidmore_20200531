91035661cfd7a1b246a4c4b0e616be50f07d6853
umgonji <umgonji@naver.com>
2020-05-31 15:04:54 +0900

    Initial commit

---
+++ app/src/main/java/dasdsa/sdn/androidmore_20200531/MainActivity.kt
@@ -0,0 +1,21 @@
+package dasdsa.sdn.androidmore_20200531
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
+}
