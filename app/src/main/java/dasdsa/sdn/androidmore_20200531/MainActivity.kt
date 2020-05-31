9144b5367abf33eadbc4e693de991f2c85660826
umgonji <umgonji@naver.com>
2020-05-31 16:23:12 +0900

    20200531 커밋

--- app/src/main/java/dasdsa/sdn/androidmore_20200531/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/androidmore_20200531/MainActivity.kt
@@ -1,7 +1,11 @@
 package dasdsa.sdn.androidmore_20200531
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,12 +13,39 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
+
+        Log.d("메일화면", "onCreate실행")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("메인화면", "onResume실행")
     }
 
     override fun setupEvents() {
+
+        moveBtn.setOnClickListener {
+            val myIntent = Intent(mContext, OtherActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 
     override fun setValues() {
+        val users = ArrayList<String>()
+        users.add("조경진")
+        users.add("홍길동")
+        users.add("김영미")
+        users.add("차오루")
+        users.add("이철수")
+
+        for (u in users) {
+            Log.d("사용자이름", u)
+        }
+
+        for (i in 0..5) {
+            Log.d("숫자반복", "${i}")
+        }
     }
 
 
---
+++ app/src/main/java/dasdsa/sdn/androidmore_20200531/OtherActivity.kt
@@ -0,0 +1,18 @@
+package dasdsa.sdn.androidmore_20200531
+
+import android.os.Bundle
+
+class OtherActivity : BaseActivity() {
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+    }
+
+}
